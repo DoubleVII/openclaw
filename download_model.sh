3e5b718465b60f65bc166a7ae020efe11533f400
double7 <33449816+DoubleVII@users.noreply.github.com>
2026-03-24 10:29:37 +0800

    Add download command for 7b model version

--- download_model.sh
+++ download_model.sh
@@ -5,6 +5,7 @@ mkdir -p ~/LLM/double7
 hf download double7/qwen2.5-3b.sft.mt.v1 --repo-type model --local-dir ~/LLM/double7/qwen2.5-3b.sft.mt.v1 --token $HF_TOKEN
 hf download double7/qwen2.5-3b.verl.genrm.v2 --repo-type model --local-dir ~/LLM/double7/qwen2.5-3b.verl.genrm.v2 --token $HF_TOKEN
 hf download double7/qwen2.5-3b.sft.mt_rm.v1 --repo-type model --local-dir ~/LLM/double7/qwen2.5-3b.sft.mt_rm.v1 --token $HF_TOKEN
+hf download double7/qwen2.5-7b.sft.mt_rm.v1 --repo-type model --local-dir ~/LLM/double7/qwen2.5-7b.sft.mt_rm.v1 --token $HF_TOKEN
 
 
 mkdir -p ~/LLM/Qwen
